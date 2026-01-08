3f2226759a46f946ff0adcc2816f05745ccbcb32
MohBendaly <133682172+MohBendaly@users.noreply.github.com>
2026-01-08 14:55:59 +0100

    Add backendUrl to environment configuration

--- sig-frontend-final/sigc-angular/src/environments/environment.ts
+++ sig-frontend-final/sigc-angular/src/environments/environment.ts
@@ -2,5 +2,7 @@
 export const environment = {
   production: false,
   mockAuth: true ,
-  apiUrl: 'http://localhost:8080/offer' // <-- DOIT pointer vers le backend (port 8080 par défaut)
+  apiUrl: 'http://localhost:8080/offer' ,
+  backendUrl: 'https://spring-pfe-ugzh.onrender.com'
+  // <-- DOIT pointer vers le backend (port 8080 par défaut)
 };
