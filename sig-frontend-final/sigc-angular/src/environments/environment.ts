4b0c6a485ea54c5eecbb2987363c3687709d86ee
MohBendaly <133682172+MohBendaly@users.noreply.github.com>
2026-01-09 10:01:52 +0100

    Fix apiUrl format in environment configuration
    Updated apiUrl to remove the trailing slash.

--- sig-frontend-final/sigc-angular/src/environments/environment.ts
+++ sig-frontend-final/sigc-angular/src/environments/environment.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: false,
   mockAuth: true ,
-  apiUrl: 'http://localhost:8080/offer' ,
+  apiUrl: 'http://localhost:8080 ,
   backendUrl: 'https://spring-pfe-ugzh.onrender.com'
   // <-- DOIT pointer vers le backend (port 8080 par défaut)
 };
