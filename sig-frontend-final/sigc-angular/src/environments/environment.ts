1b07f431e49cff2d78324698ca6d6e8666935d7f
MohBendaly <133682172+MohBendaly@users.noreply.github.com>
2026-01-09 10:23:32 +0100

    Change appName from 'SIGC Application' to 'Angular Application'

--- sig-frontend-final/sigc-angular/src/environments/environment.ts
+++ sig-frontend-final/sigc-angular/src/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
   apiUrl: 'http://localhost:8080/offer',
   backendUrl: 'http://localhost:8080',
   version: '1.0.0',
-  appName: 'SIGC Application'
+  appName: 'Angular Application'
 };
